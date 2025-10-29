7e5fd295645e0914ba43b6ef4d09148c62825b5f
Bonvas <bonvas73@gmail.com>
2025-10-29 07:41:48 +0200

    Add file exercise1

---
+++ exercise1.py
@@ -0,0 +1,17 @@
+# declare three variables (name, price, stock status) for a product using snake_case.
+# print declared variables and their type.
+# in a single line of code, assign the values 20, 58, and 97 to the variables x, y, and z.
+# pring variables x, y, z.
+
+product_name = "Laptop"
+product_price = 1200.99
+is_in_stock = True
+
+print(product_name)
+print(product_price)
+print(type(product_name))
+print(type(product_price))
+
+x, y, z = 20, 58, 97
+print(x, y, z)
+
