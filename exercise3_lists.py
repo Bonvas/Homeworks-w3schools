ce640a1240cf25ae2d6dbd34a1b3681ed57b1459
Bonvas <bonvas73@gmail.com>
2025-10-29 07:43:11 +0200

    Add file exercise3_lists

---
+++ exercise3_lists.py
@@ -0,0 +1,71 @@
+# creat different lists with data and print it
+
+# second option for first list: city_1 = city_2 = city_3 = "Lviv":
+name_1 = "Vasyl"
+age_1 = 52
+city_1 = "Lviv"
+
+name_2 = "Daniel"
+age_2 = 23
+city_2 = "Lviv"
+
+name_3 = "George"
+age_3 = 20
+city_3 = "Lviv"
+
+list_men = [name_1, age_1, city_1, name_2, age_2, city_2, name_3, age_3, city_3]
+print(list_men)
+print(type(list_men))
+
+a = "apple"
+b = "banana"
+o = "orange"
+p = "pear"
+l = "lemon"
+g = "grapes"
+w = "watermelon"
+k = "kiwi"
+
+fruits = [a, b, o, p, l, g, w, k]
+print(fruits)
+print(type(fruits))
+
+num_1 = 10
+number = 34.75
+bool = True
+num_2 = 0.4587
+x = "Spring"
+
+any_list = [num_1, number, bool, num_2, x]
+print(any_list)
+print(type(any_list))
+
+col_1 = "Red"
+col_2 = "White"
+col_3 = "Yellow"
+col_4 = "Green"
+col_5 = "Black"
+col_6 = "Blue"
+
+list_color = [col_1, col_2, col_3, col_4, col_5, col_6]
+print(list_color)
+print(type(list_color))
+
+word_1 = "Python"
+word_2 = "lists"
+word_3 = "are"
+word_4 = "powerful."
+
+sentence = [word_1, word_2, word_3, word_4]
+print(sentence)
+print(type(sentence))
+print(word_1, word_2, word_3, word_4)
+
+# create blank list
+n = []
+print(n)
+print(type(n))
+
+
+
+
