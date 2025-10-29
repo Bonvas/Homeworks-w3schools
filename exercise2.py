3538597cdaf93eab3e7de143dae57990ed8fba15
Bonvas <bonvas73@gmail.com>
2025-10-29 07:42:31 +0200

    Add file exercise2

---
+++ exercise2.py
@@ -0,0 +1,22 @@
+# create a list, print it and its data type
+
+name = "Vasyl"
+age = 52
+city = "Lviv"
+
+personal_info = [name, age, city]
+
+print(personal_info)
+print(type(personal_info))
+
+# Four variables are given representing the prices of 
+# different goods. Create list and print him and type.
+
+price_a = 15.50
+price_b = 40
+price_c = 7.99
+price_d = 135.00
+
+product_prices = [price_a, price_b, price_c, price_d]
+print(product_prices)
+print(type(product_prices))
